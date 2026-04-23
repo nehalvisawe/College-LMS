7f75432a0626e4ebc4a4765d7a45f81180ef002a
nehalvisawe <nehalvisawe28@gmail.com>
2026-04-23 19:03:24 +0530

    solve the errors in better-auth configuration

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL ?? "https://college-lms-system.vercel.app/teachers",
+  baseURL: process.env.BETTER_AUTH_URL ?? "https://college-lms-system.vercel.app",
   plugins: []
 });
